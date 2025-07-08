7310b3fe8aa447f8c81cd6d75487474541e60d93
shafaqatgithub <shafaqatmitza.jcer@gmail.com>
2025-07-08 16:50:08 +0530

    testimonials removed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -745,7 +745,7 @@ const HomePage = () => {
       </section>
 
       {/* Testimonials Section */}
-      <section className="py-20 bg-white">
+      {/* <section className="py-20 bg-white">
         <div className="container mx-auto px-4">
           <div className="text-center mb-16 fade-in-up">
             <h2 className="text-4xl md:text-5xl font-bold mb-6 text-gray-900">
@@ -790,7 +790,7 @@ const HomePage = () => {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Contact Section */}
       <section id="contact" className="py-20 bg-gray-50">
